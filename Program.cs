df209b2d0e61892a04b74d98adbe89c8376509e2
AleksandrAlexashin <sashokmetall@mail.ru>
2022-01-27 19:22:43 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab21
+{
+    class Program
+    {
+          static int[,] pole;
+          const int m=10;
+           const int n=5;
+        static void Main(string[] args)
+        {
+
+            pole = new int[n, m];
+
+            Thread gardener1 = new Thread(sad1);
+            Thread gardener2 = new Thread(sad2);
+
+            gardener1.Start();
+            gardener2.Start();
+
+            gardener1.Join();
+            gardener2.Join();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(pole[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+         static void sad1()
+         {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (pole[i, j] == 0)
+                        pole[i, j] = 1;
+                    Thread.Sleep(1);
+                }
+            }
+         }
+
+         static void sad2()
+         {
+            for (int i = m - 1; i > 0; i--)
+            {
+                for (int j = n - 1; j > 0; j--)
+                {
+                    if (pole[j, i] == 0)
+                        pole[j, i] = 2;
+                    Thread.Sleep(20);
+                }
+
+
+            }
+         }
+    }
+}
